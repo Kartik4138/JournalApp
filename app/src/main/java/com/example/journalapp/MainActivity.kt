3a3c233ce87cc3ef44b15e8e59e27ad439311b49
Kartik Singh <kartiksingh.7.rs@gmail.com>
2024-09-19 02:44:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.journalapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.journalapp.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Btnregister.setOnClickListener(){
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+
+}
